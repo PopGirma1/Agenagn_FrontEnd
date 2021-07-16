9245b1b9a4e8ef2631ca303a276e2d1c3d4a3dc3
desalegn12 <desalegnmihret97@gmail.com>
2021-07-16 22:50:45 +0300

    search componet is aadded and css enhancement

--- src/homePage/homePage.js
+++ src/homePage/homePage.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
 import "./homePage.css";
 
 class HomePage extends Component {
   render() {
     return (
       <div className="nav">
-        <div className="content">
-          <NaveBare classname="home">Home</NaveBare>
-          <NaveBare classname="home">Registor</NaveBare>
-          <NaveBare classname="home">About Us</NaveBare>
-          <NaveBare classname="home">Login</NaveBare>
+      <div className="homepage">
+        <Router>
+          <Route exact={true} path="/">
+            <div className="nav">
+              <div className="float-right">
+                <h1>AGENAGN</h1>
+                <Search hint='search house' classname="searchbar" />
+              </div>
+              <div className="content">
+                <NaveBare classname="home">Home</NaveBare>
+                <NaveBare classname="home">Registor</NaveBare>
+                <NaveBare classname="home">
+                  <Link to="/about" className="about-link">
+                    About Us
+                  </Link>
+                </NaveBare>
+                <NaveBare classname="home">Login</NaveBare>
+              </div>
+            </div>
+          </Route>
+         
+        </Router>
+        <div className='left-content'>
+          
         </div>
       </div>
     );
@@ -23,6 +46,11 @@ const NaveBare = ({ classname, children, onclick }) => (
   <button type="button" className={classname}>
     {children}
   </button>
+);
+
+
+const Search = ({ classname, hint }) => (
+  <input placeholder={hint} className={classname} />
 );
 /*make export default means someone imort is the name you want */
 export default HomePage;
