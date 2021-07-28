907e557ed07eaa217ca590fa293ab5a46cf1380d
PopGirma1 <girmaytadesepop@gmail.com>
2021-07-28 14:50:07 +0300

    home registration form added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import AboutUs from "./component/AboutUs";
 import HomePage from "./component/HomePage";
 import Login from "./component/Login";
 import SignUp from "./component/SignUp";
+import Filter from "./component/Filter";
+
 
 export default function App () {
     return(
         <div>
-            <Post/>
+            {/*<Post/>*/}
+            <Filter/>
             {/*<CommentJs/>*/}
             {/*<Detail/>*/}
             {/*<AboutUs/>*/}
-            {/*<HomePage/>*/}
+            {/*<HomeRegistrationForm/>*/}
             {/*<Login/>*/}
             {/*<SignUp/>*/}
 
