fdb25239383e52b767b58163bdd034e6bfd3897c
desalegn12 <desalegnmihret97@gmail.com>
2021-08-04 20:34:25 +0300

    sign in page is completed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,48 @@
+import React from "react";
+import { Link, Route, HashRouter, BrowserRouter } from "react-router-dom";
+import { withStyles, Container } from "@material-ui/core";
 
-import React from 'react';
-import {Link, Route, HashRouter, BrowserRouter} from "react-router-dom";
-import {withStyles, Container} from "@material-ui/core";
+import NavTabs from "./components/NavTabs";
+import MainBody from "./pages/Home";
+import Detail from "./pages/Details/";
+import LoginRegister from "./pages/Authentication/LoginRegister";
+import UserHome from "./pages/Authentication/HomeRegistrationForm/UserHome";
+import Footer from "./components/Footer";
+import Comment from "./pages/Comment";
 
-import NavTabs from './components/NavTabs'
-import MainBody from './pages/Home'
-import Detail from './pages/Details/'
-import LoginRegister from './pages/Authentication/LoginRegister'
-import UserHome from './pages/Authentication/HomeRegistrationForm/UserHome'
-import Footer from './components/Footer'
-import Comment from './pages/Comment'
-
-const useStyles = ((theme) => ({
-    root: {
-
-    },
-    mainParts: {
-        marginTop: '100px'
-    },
-}));
+const useStyles = (theme) => ({
+	root: {},
+	mainParts: {
+		marginTop: "100px",
+	},
+});
 
 class App extends React.Component {
-    render() {
-        const {classes} = this.props;
+	render() {
+		const { classes } = this.props;
 
-        return (
-            <div className={classes.root}>
-
-                <BrowserRouter basename={process.env.PUBLIC_URL}>
-                    <NavTabs/>
-                    <div className={classes.mainParts}>
-                        <Route  path='/' exact component={MainBody}/>
-                        <Route  path={process.env.PUBLIC_URL + '/detail'} component={Detail}/>
-                        <Route  path={process.env.PUBLIC_URL + '/login'} component={LoginRegister}/>
-                        <Route  path='/home' component={UserHome}/>
-                        <Route  path='/comment' component={Comment}/>
-                    </div>
-                </BrowserRouter >
-                <Footer/>
-
-            </div>
-        );
-    }
-
-
-
+		return (
+			<div className={classes.root}>
+				<BrowserRouter basename={process.env.PUBLIC_URL}>
+					<NavTabs />
+					<div className={classes.mainParts}>
+						<Route path="/" exact component={MainBody} />
+						<Route
+							path={process.env.PUBLIC_URL + "/detail"}
+							component={Detail}
+						/>
+						<Route
+							path={process.env.PUBLIC_URL + "/login"}
+							component={LoginRegister}
+						/>
+						<Route path="/home" component={UserHome} />
+						<Route path="/comment" component={Comment} />
+					</div>
+				</BrowserRouter>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default withStyles(useStyles)(App);
